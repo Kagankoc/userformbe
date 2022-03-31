c5ec6fd1adc9616135bd9b7bc1309c5459223215
Cemsu Kağan KOÇ <cemsukagan@hotmail.com>
2022-03-31 22:25:52 +0300

    cors

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
 const express= require('express');
+const cors = require("cors");
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
 const app = express();
 
 app.use(express.json());
-
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 app.use('/',authRoutes);
 app.use('/',userRoutes);
 const listener = app.listen(process.env.PORT || 3000, () => {
